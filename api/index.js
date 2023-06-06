475a743ca7befdc4e8c45cb17f57dbfe74b5e365
Cemb93 <karloz13m@gmail.com>
2023-06-05 22:57:44 -0500

    Iniciando servidor de repaso

---
+++ api/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const { PORT } = process.env;
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Listening at PORT:`, 3001);
+  });
+});
---
+++ api/src/models/Character.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("character", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('Alive', 'Dead', 'unknown'),
+      allowNull: false,
+    },
+    //* Este sería mi personaje
+    mine: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  }, {
+    timestamps: false,
+  });
+};
